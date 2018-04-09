84462f158f05cf443b1a2a6a8d85321946cc30a1
ygroxie <38132498+ygroxie@users.noreply.github.com>
2018-04-10 01:28:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ bot.on('message', function (message) {
 
 bot.on('message', function (message) {
     if (message.content === '!birthday') {
-        message.channel.tag('pong !')
+        message.channel.tag.send('pong !')
     }
 });
 
