b6136a7a27f39787ac798fef8e8aaf83fe286c77
ygroxie <38132498+ygroxie@users.noreply.github.com>
2018-05-05 03:01:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
+var prefix = (".")
+
 bot.on("ready", function() {
     bot.user.setGame("YgroxBot, !help");
     console.log("Le bot a bien, était connecté !");
