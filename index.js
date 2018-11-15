d9023ba37a6c5faf14958b7ec7180a8903a66419
ygroxie <38132498+ygroxie@users.noreply.github.com>
2018-11-15 22:02:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ bot.on('message',(message)=>{
     }
     })
 
-bot.on('message',guild=>{
+bot.on('guild',message =>{
     if (message.content == "?membres") {
         message.reply(`Nous sommes actuellement ${member.guild.memberCount} membres sur le discord.`)
     }
